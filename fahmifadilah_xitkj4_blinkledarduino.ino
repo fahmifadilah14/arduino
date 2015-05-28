3475354b0bd70df6db4e00d4e1f4e656b4b2b3d3
fahmifadilah14 <fahmifadilah14@users.noreply.github.com>
2015-05-28 10:38:49 +0700

    Update fahmifadilah_xitkj4_blinkledarduino.ino

--- fahmifadilah_xitkj4_blinkledarduino.ino
+++ fahmifadilah_xitkj4_blinkledarduino.ino
@@ -1,14 +1,14 @@
 int varled3 = 3;
-int varled4 = 4;
-int varled5 = 5;
-int varled6 = 6;
+int varled4 = 5;
+int varled5 = 6;
+int varled6 = 7;
 
 void setup() {
 
   pinMode(varled3, OUTPUT);
-  pinMode(varled4, OUTPUT);
   pinMode(varled5, OUTPUT);
   pinMode(varled6, OUTPUT);
+  pinMode(varled7, OUTPUT);
 }
 
 void loop() {
@@ -16,11 +16,6 @@ void loop() {
   {digitalWrite(varled3, HIGH);
   delay(100);
   digitalWrite(varled3, LOW);
-  delay(100);}
-  
-  {digitalWrite(varled4, HIGH);
-  delay(100);
-  digitalWrite(varled4, LOW);
   delay(100);}
   
   {digitalWrite(varled5, HIGH);
@@ -31,5 +26,10 @@ void loop() {
   {digitalWrite(varled6, HIGH);
   delay(100);
   digitalWrite(varled6, LOW);
+  delay(100);}
+  
+  {digitalWrite(varled7, HIGH);
+  delay(100);
+  digitalWrite(varled7, LOW);
   delay(100);}
 }
